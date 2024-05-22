715751fb9965c3cca4cc6c61e67a4a6713aca05f
MarlonPelau <MarlonRegis@pursuit.org>
2024-05-22 15:31:45 -0400

    methods push and pop implemented in Stack file

--- Stack.js
+++ Stack.js
@@ -5,5 +5,19 @@ class Stack {
     this.top = null;
   }
 
-  
+  push(data) {
+    const newNode = new Node(data);
+    newNode.next = this.top;
+    this.top = newNode;
+  }
+
+  pop() {
+    if (this.isEmpty()) return null;
+    const poppedNode = this.top;
+    this.top = this.top.next;
+    return poppedNode.data;
+  }
+
 }
+
+module.exports = Stack;
