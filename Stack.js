959e4b2453180a6f159b236055aede9fe9fba6e5
MarlonPelau <MarlonRegis@pursuit.org>
2024-05-22 15:42:31 -0400

    the rest of methods, size, findMin and sort added

--- Stack.js
+++ Stack.js
@@ -26,8 +26,39 @@ class Stack {
   isEmpty() {
     return this.top === null;
   }
-  
 
+  size() {
+    let count = 0;
+    let current = this.top;
+    while (current) {
+      count++;
+      current = current.next;
+    }
+    return count;
+  }
+
+  findMin() {
+    if (this.isEmpty()) return null;
+    let min = this.top.data;
+    let current = this.top.next;
+    while (current) {
+      if (current.data < min) min = current.data;
+      current = current.next;
+    }
+    return min;
+  }
+  sort() {
+    if (this.isEmpty()) return;
+    const tempStack = new Stack();
+    while (!this.isEmpty()) {
+      const temp = this.pop();
+      while (!tempStack.isEmpty() && tempStack.peek() > temp) {
+        this.push(tempStack.pop());
+      }
+      tempStack.push(temp);
+    }
+    this.top = tempStack.top;
+  }
 }
 
 module.exports = Stack;
